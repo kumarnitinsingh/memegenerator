305a8505281ea6c445bd275c64eabd9db3f4399c
kumarnitinsingh <nitin962141@gmail.com>
2020-06-14 20:51:11 +0530

    memegenerator

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://is2-ssl.mzstatic.com/image/thumb/Purple62/v4/6a/82/17/6a82179d-6fb0-3ff7-cf94-add61e2344ae/mzl.koixrqef.png/246x0w.png" 
+                alt="Problem?"
+            />
+            <p>MemeBajjar</p>
+        </header>
+    )
+}
+
+export default Header
