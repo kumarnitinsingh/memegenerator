d7ecf237e71c2a9165fd1e298f921aae6af472ad
kumarnitinsingh <nitin962141@gmail.com>
2020-06-14 20:57:56 +0530

    Updates

---
+++ precache-manifest.8aad4aae318130088cf100af83ec141e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22bb743065acffeddcf3eb019a79c44",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "9630898194a708d9e0c7",
+    "url": "/memegenerator/static/css/main.f394d5de.chunk.css"
+  },
+  {
+    "revision": "97652af3cbadbb8de8a5",
+    "url": "/memegenerator/static/js/2.b90fae72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegenerator/static/js/2.b90fae72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9630898194a708d9e0c7",
+    "url": "/memegenerator/static/js/main.0865dd89.chunk.js"
+  },
+  {
+    "revision": "d772c24e0a973e433a65",
+    "url": "/memegenerator/static/js/runtime-main.fe740041.js"
+  }
+]);
